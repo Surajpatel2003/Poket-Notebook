ab69a857da806fe5988e6f99f9fc638b076696b6
Surajpatel2003 <83915532+Surajpatel2003@users.noreply.github.com>
2024-01-15 00:56:07 +0530

    Succefully integrate jwt token and hasing , salt the password using bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { Schema } = require("mongoose");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "iamsuraj2003";
 
 // Create user using:POST "/api/auth/createuser". No login required
 router.post(
@@ -31,11 +35,22 @@ router.post(
           erorr: "Sorry a user with this email adress is already exist",
         });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
       // .then((user) => res.json(user))
       // .catch((error) => {
       //   res.send({
@@ -43,7 +58,6 @@ router.post(
       //     error: error.message,
       //   });
       // });
-      res.json(user);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error is occure");
